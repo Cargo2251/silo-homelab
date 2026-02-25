c360d0c9795d0cb650b1f3108145fed7c1785797
Vlad <vladsargu@hotmail.co.uk>
2026-02-25 14:44:39 +0000

    Feat: Made it so the script can now check on all the containers, making it fully self-healing

--- scripts/health_checkup.sh
+++ scripts/health_checkup.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-CONTAINER_NAME=npm
-COMPOSE_DIR=~/docker/npm
+CONTAINERS_NAMES=("npm" "diun" "nextcloud" "dockge" "dozzle" "couchdb" "uptime-kuma" "vaultwarden" "homepage" "speedtest-tracker")
+COMPOSE_DIR=~/docker
 MAIN_LOG=~/homelab_monitor.log
 
-if [ "$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME)" = "true" ]; then
-    echo "$(date) - The $CONTAINER_NAME is running" >> "$MAIN_LOG"
-    exit
-else
-    echo "$(date) - The $CONTAINER_NAME is not running, attempting to restart" >> "$MAIN_LOG"
-    cd $COMPOSE_DIR || exit 1;
+for CONTAINER in "${CONTAINERS_NAMES[@]}"; do
 
-    if ! docker compose up -d; then
-        echo "$(date) - Restart failed! Grabbing logs.." >> "$MAIN_LOG"
+    IS_RUNNING=$(docker container inspect -f '{{.State.Running}}' "$CONTAINER" 2>/dev/null)
 
-        docker logs "$CONTAINER_NAME" --tail 50 > ~/homelab_${CONTAINER_NAME}_error.log
+    if [ "$IS_RUNNING" = "true" ]; then
+        echo "$(date) - The ${CONTAINER} is running" >> "$MAIN_LOG"
     else
-        echo "$(date) - Ran compose up, container should be up shortly."
+        echo "$(date) - The ${CONTAINER} is not running, attempting to restart" >> "$MAIN_LOG"
+        cd $COMPOSE_DIR || exit 1;
 
+        if ! docker compose up -d; then
+            echo "$(date) - Restart failed! Grabbing logs.." >> "$MAIN_LOG"
+
+            docker logs "$CONTAINER" --tail 50 > ~/homelab_${CONTAINER}_error.log
+        else
+            echo "$(date) - Ran compose up, container should be up shortly." >> "$MAIN_LOG"
+
+        fi
     fi
-fi
+done
