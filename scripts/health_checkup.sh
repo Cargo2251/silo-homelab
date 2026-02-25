484a5d43a08479ce925cf783e23428b3d57c8cd2
Vlad <vladsargu@hotmail.co.uk>
2026-02-25 11:55:40 +0000

    fix: add exit catch as per shellcheck && added error handling and logs output if the container fails to restart.

--- scripts/health_checkup.sh
+++ scripts/health_checkup.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-CONTAINER_NAME=test-web
-COMPOSE_DIR=~/docker/test-web
+CONTAINER_NAME=npm
+COMPOSE_DIR=~/docker/npm
 MAIN_LOG=~/homelab_monitor.log
 
 if [ "$( docker container inspect -f '{{.State.Running}}' $CONTAINER_NAME)" = "true" ]; then
-echo "$(date) - The $CONTAINER_NAME is running" >> $MAIN_LOG
-exit
+    echo "$(date) - The $CONTAINER_NAME is running" >> "$MAIN_LOG"
+    exit
 else
-echo "$(date) - The $CONTAINER_NAME is not running, attempting to restart" >> $MAIN_LOG
-cd $COMPOSE_DIR; docker compose up -d
-echo "Ran compose up, container should be up shortly" >> $MAIN_LOG
+    echo "$(date) - The $CONTAINER_NAME is not running, attempting to restart" >> "$MAIN_LOG"
+    cd $COMPOSE_DIR || exit 1; docker compose up -d
+
+    if [ $? -ne 0 ]; then
+        echo "$(date) - Restart failed! Grabbing logs.." >> "$MAIN_LOG"
+
+        docker logs "$CONTAINER_NAME" --tail 50 > ~/homelab_${CONTAINER_NAME}_error.log
+    else
+        echo "$(date) - Ran compose up, container should be up shortly."
+
+    fi
 fi
